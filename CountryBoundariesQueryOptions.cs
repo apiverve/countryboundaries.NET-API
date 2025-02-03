c0e59ad63cdd59b08ff394a2a5aadc25614d7410
APIVerve Team <hello@apiverve.com>
2025-02-03 15:33:30 -0600

    Minor Version Update

---
+++ CountryBoundariesQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class CountryBoundariesQueryOptions {
+public string name { get; set; }
+}
+
+}
